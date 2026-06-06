21f612f9cab5ff4f05e60b3fbdd5ccde7928ed47
adurak <durakahmet049@gmail.com>
2026-06-06 14:45:54 +0200

    fix: set search_path=public for Neon PostgreSQL 15 compatibility

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,3 +1,6 @@
 import { Pool } from 'pg'
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+
+// Neon (PostgreSQL 15+) doesn't set search_path by default
+pool.on('connect', client => { client.query('SET search_path TO public') })
